c091364c3d27f6b944bbc2f420d23a1f09dbc405
lili <tulilirockz.pub+git@gmail.com>
2023-06-28 23:11:20 -0300

    feat: add ubuntu and inter fonts + fix slow cloning for nerdfonts
    fixes: [fonts] can we repackage Nerdfonts #3 [nix] install and uninstall scripts #2

--- Containerfile
+++ Containerfile
@@ -1,8 +1,8 @@
 FROM registry.fedoraproject.org/fedora:latest AS builder
 
-RUN dnf update -y && dnf install --disablerepo='*' --enablerepo='fedora,updates' --setopt install_weak_deps=0 --nodocs --assumeyes wget && dnf clean all
+RUN dnf update -y && dnf install --disablerepo='*' --enablerepo='fedora,updates' --setopt install_weak_deps=0 --nodocs --assumeyes git wget unzip && dnf clean all
 
-RUN mkdir -p /tmp/ublue-os/files/{etc,usr}
+RUN mkdir -p /tmp/ublue-os/files/{etc,usr} && mkdir /tmp/ublue-os/rpms
 
 COPY usr /tmp/ublue-os/files/usr
 COPY etc /tmp/ublue-os/files/etc
--- fetch.sh
+++ fetch.sh
@@ -2,15 +2,25 @@
 set -euo pipefail
 FILES_ROOT="/tmp/ublue-os/files"
 
-git clone 'https://github.com/ryanoasis/nerd-fonts' /tmp/nerdfonts
-mv /tmp/nerdfonts/patched-fonts/* "$FILES_ROOT/usr/share/fonts"
-
 git clone 'https://github.com/dnkmmr69420/nix-installer-scripts' /tmp/nix-installer-scripts
-mv /tmp/nix-installers-scripts/installer-scripts/silverblue-nix-installer.sh "$FILES_ROOT/usr/bin/ublue-nix-install"
-mv /tmp/nix-installers-scripts/uninstaller-scripts/silverblue-nix-uninstaller.sh "$FILES_ROOT/usr/bin/ublue-nix-uninstall"
+install -c -m 0755 /tmp/nix-installer-scripts/installer-scripts/silverblue-nix-installer.sh "$FILES_ROOT/usr/bin/ublue-nix-install"
+install -c -m 0755 /tmp/nix-installer-scripts/uninstaller-scripts/silverblue-nix-uninstaller.sh "$FILES_ROOT/usr/bin/ublue-nix-uninstall"
 
 wget -O /tmp/devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64"
 install -c -m 0755 /tmp/devpod "$FILES_ROOT/usr/bin"
 
 wget -O /tmp/devpod.rpm https://github.com/loft-sh/devpod/releases/latest/download/DevPod_linux_x86_64.rpm 
 mv /tmp/devpod.rpm /tmp/ublue-os/rpms
+
+wget -O /tmp/inter.zip 'https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip'
+mkdir /tmp/inter
+unzip /tmp/inter.zip -d "/tmp/inter"
+mv "/tmp/inter/Inter Desktop/" "$FILES_ROOT/usr/share/fonts"
+
+wget -O /tmp/ubuntu.zip 'https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip'
+unzip /tmp/ubuntu.zip -d "/tmp/ubuntu"
+rm -rf /tmp/ubuntu/__MACOSX
+mv /tmp/ubuntu/* "$FILES_ROOT/usr/share/fonts"
+
+git clone --depth 1 'https://github.com/ryanoasis/nerd-fonts' /tmp/nerdfonts
+mv /tmp/nerdfonts/patched-fonts/* "$FILES_ROOT/usr/share/fonts"
