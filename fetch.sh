1f8f8fa703b7340015b0d3f96fb7628c234649cc
tulili <tulilirockz.pub+git@gmail.com>
2023-07-17 11:05:20 -0300

    fix(workaround): fonts not being properly displayed in systems

--- fetch.sh
+++ fetch.sh
@@ -12,17 +12,6 @@ install -c -m 0755 /tmp/devpod "$FILES_ROOT/usr/bin"
 wget -O /tmp/devpod.rpm https://github.com/loft-sh/devpod/releases/latest/download/DevPod_linux_x86_64.rpm 
 mv /tmp/devpod.rpm /tmp/ublue-os/rpms
 
-wget -O /tmp/inter.zip 'https://github.com/rsms/inter/releases/download/v3.19/Inter-3.19.zip'
-mkdir /tmp/inter
-unzip /tmp/inter.zip -d "/tmp/inter"
-mv "/tmp/inter/Inter Desktop/" "$FILES_ROOT/usr/share/fonts"
-
-wget -O /tmp/ubuntu.zip 'https://assets.ubuntu.com/v1/0cef8205-ubuntu-font-family-0.83.zip'
-unzip /tmp/ubuntu.zip -d "/tmp/ubuntu"
-rm -rf /tmp/ubuntu/__MACOSX
-chmod 755 /tmp/ubuntu/*
-mv /tmp/ubuntu/* "$FILES_ROOT/usr/share/fonts"
-
 wget -O /tmp/intelmono.zip 'https://github.com/intel/intel-one-mono/releases/latest/download/ttf.zip'
 unzip /tmp/intelmono.zip -d "/tmp/intelmono"
 mv /tmp/intelmono/ttf "$FILES_ROOT/usr/share/fonts/intel-one-mono"
