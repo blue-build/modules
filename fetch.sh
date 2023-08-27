96ef1b1efc8fa1d7ba920f56efd95a542500a9a4
tulili <tulilirockz.pub+git@gmail.com>
2023-08-27 00:50:06 -0300

    refactor(nix): use determinate system installer instead of custom script

--- fetch.sh
+++ fetch.sh
@@ -2,10 +2,6 @@
 set -euo pipefail
 FILES_ROOT="/tmp/ublue-os/files"
 
-git clone 'https://github.com/dnkmmr69420/nix-installer-scripts' /tmp/nix-installer-scripts
-install -c -m 0755 /tmp/nix-installer-scripts/installer-scripts/silverblue-nix-installer.sh "$FILES_ROOT/usr/bin/ublue-nix-install"
-install -c -m 0755 /tmp/nix-installer-scripts/uninstaller-scripts/silverblue-nix-uninstaller.sh "$FILES_ROOT/usr/bin/ublue-nix-uninstall"
-
 wget -O /tmp/devpod "https://github.com/loft-sh/devpod/releases/latest/download/devpod-linux-amd64"
 install -c -m 0755 /tmp/devpod "$FILES_ROOT/usr/bin"
 
