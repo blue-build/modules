0bf095c3e17bcb19c60dfc9658de9e98b7f81b5e
lorduskordus <pavel.kordovsky@gmail.com>
2023-09-13 10:36:01 +0200

    module bling: fix dconf-update-service

--- modules/bling/installers/dconf-update-service.sh
+++ modules/bling/installers/dconf-update-service.sh
@@ -3,5 +3,5 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-cp -r "$BLING_DIRECTORY/files/usr/etc/systemd/system/dconf-update.service" "/usr/etc/systemd/system/dconf-update.service"
+cp -r "$BLING_DIRECTORY/files/usr/lib/systemd/system/dconf-update.service" "/usr/lib/systemd/system/dconf-update.service"
 systemctl enable dconf-update.service
