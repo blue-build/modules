be8f58a03580a7605e5d5ab0e2106715bf9824d2
zelikos <pcsikos@zelikos.dev>
2023-09-15 14:11:06 -0400

    fix(systemd): Run disables before enables

--- modules/systemd/systemd.sh
+++ modules/systemd/systemd.sh
@@ -9,27 +9,27 @@ get_yaml_array USER_ENABLED '.user.enabled[]' "$1"
 get_yaml_array USER_DISABLED '.user.disabled[]' "$1"
 
 
-if [[ ${#ENABLED[@]} -gt 0 ]]; then
-    for unit in "${ENABLED[@]}"; do
-        unit=$(printf "$unit")
-        systemctl enable $unit
-    done
-fi
 if [[ ${#DISABLED[@]} -gt 0 ]]; then
     for unit in "${DISABLED[@]}"; do
         unit=$(printf "$unit")
         systemctl disable $unit
     done
 fi
-if [[ ${#USER_ENABLED[@]} -gt 0 ]]; then
-    for unit in "${USER_ENABLED[@]}"; do
+if [[ ${#ENABLED[@]} -gt 0 ]]; then
+    for unit in "${ENABLED[@]}"; do
         unit=$(printf "$unit")
-        systemctl --user enable $unit
+        systemctl enable $unit
     done
 fi
 if [[ ${#USER_DISABLED[@]} -gt 0 ]]; then
     for unit in "${USER_DISABLED[@]}"; do
         unit=$(printf "$unit")
         systemctl --user disable $unit
+    done
+fi
+if [[ ${#USER_ENABLED[@]} -gt 0 ]]; then
+    for unit in "${USER_ENABLED[@]}"; do
+        unit=$(printf "$unit")
+        systemctl --user enable $unit
     done
 fi
