d9d70bbaf917897a09050693172db14666221a56
Tulili <tulilirockz.pub+git@gmail.com>
2023-09-20 23:45:19 -0300

    fix(bling,container-tools): incorrect quoting in rpm statement

--- modules/bling/installers/container-tools.sh
+++ modules/bling/installers/container-tools.sh
@@ -7,5 +7,5 @@ install -c -m 0755 "$BLING_DIRECTORY/files/usr/bin/docker-compose" "/usr/bin/doc
 install -c -m 0755 "$BLING_DIRECTORY/files/usr/bin/kind" "/usr/bin/kind"
 install -c -m 0755 "$BLING_DIRECTORY/files/usr/bin/kubectx" "/usr/bin/kubectx"
 install -c -m 0755 "$BLING_DIRECTORY/files/usr/bin/kubectx" "/usr/bin/kubens" 
-rpm-ostree install "$BLING_DIRECTORY/rpms/dive*.rpm"
+rpm-ostree install "$BLING_DIRECTORY"/rpms/dive*.rpm
 
