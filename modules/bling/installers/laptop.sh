14db12cab90d97d8191fd3f1254a022d613d03cd
tulili <tulilirockz.pub+git@gmail.com>
2023-09-25 09:24:34 -0300

    fix(bling,laptop): check first if power-profiles-daemon is installed

--- modules/bling/installers/laptop.sh
+++ modules/bling/installers/laptop.sh
@@ -5,7 +5,12 @@ set -oue pipefail
 
 wget "https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-$(rpm -E %fedora)/ublue-os-staging-fedora-$(rpm -E %fedora).repo" \
     -O "/etc/yum.repos.d/_copr_ublue-os_staging.repo"
-rpm-ostree remove power-profiles-daemon --install fprintd tlp tlp-rdw
+
+if rpm -qa | grep power-profiles-daemon ; then
+    rpm-ostree remove power-profiles-daemon --install=fprintd --install=tlp --install=tlp-rdw
+else
+    rpm-ostree install fprintd tlp tlp-rdw
+fi
 systemctl enable tlp
 systemctl enable fprintd
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_ublue-os_staging.repo
