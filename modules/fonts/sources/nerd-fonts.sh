0e18b131c8524ec01a2e5fef1019052d570571db
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2023-09-30 07:54:37 -0300

    chore: fonts module

---
+++ modules/fonts/sources/nerd-fonts.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -oue pipefail
+
+FONTS=( "$@" )
+URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/"
+DIR_PRINCIPAL=/usr/share/fonts/nerd-fonts
+COMPACT_FORMAT="zip"
+
+# To download nerd-fonts you need to enter the name of the font and the format you want. See the 3rd parameter of the download script.
+
+if [ ${#FONTS[@]} -gt 0 ]; then
+
+    echo "Installation of nerd-fonts started"
+
+    for font in "${FONTS[@]}"; do
+
+        font="$(echo "$font" | sed -e 's|^[[:blank:]]||g' | tr -d '\n')"
+        
+        bash "$FONTS_MODULE_SCRIPTS_DIR"/download.sh "$font" "$COMPACT_FORMAT" "$URL$font.$COMPACT_FORMAT" "$DIR_PRINCIPAL/$font" 
+
+    done
+
+    fc-cache -f $DIR_PRINCIPAL
+
+fi
