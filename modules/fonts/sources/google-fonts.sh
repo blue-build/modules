56c5a22de2ebd3a5097e440476d587d6bd71dda1
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2023-10-01 07:06:51 -0300

    chore: scripts

--- modules/fonts/sources/google-fonts.sh
+++ modules/fonts/sources/google-fonts.sh
@@ -4,7 +4,7 @@ set -oue pipefail
 mapfile -t FONTS <<< "$@"
 URL="https://fonts.google.com/download?family="
 DIR_PRINCIPAL=/usr/share/fonts/google-fonts
-FORMAT="zip"
+COMPACT_FORMAT="zip"
 
 # To download google-fonts it is necessary to enter the name of the font replacing the spaces with '%20'. See the 3rd parameter of the download script.
 
@@ -12,12 +12,13 @@ if [ ${#FONTS[@]} -gt 0 ]; then
 
     echo "Installation of google-fonts started"
 
+    rm -rf "$DIR_PRINCIPAL"
+
     for font in "${FONTS[@]}"; do
 
         font="$(echo "$font" | sed -e 's|^[[:blank:]]||g' | tr -d '\n')"
 
-        rm -rf "$DIR_PRINCIPAL/$font"
-        bash "$FONTS_MODULE_SCRIPTS_DIR"/download.sh "$font" "$FORMAT" "$URL${font// /%20}" "$DIR_PRINCIPAL/$font" 
+        bash "$FONTS_MODULE_SCRIPTS_DIR"/download.sh "$font" "$COMPACT_FORMAT" "$URL${font// /%20}" "$DIR_PRINCIPAL/$font" 
 
     done
 
--- modules/fonts/sources/nerd-fonts.sh
+++ modules/fonts/sources/nerd-fonts.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-FONTS=( "$@" )
+mapfile -t FONTS <<< "$@"
 URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/"
 DIR_PRINCIPAL=/usr/share/fonts/nerd-fonts
 COMPACT_FORMAT="zip"
@@ -12,11 +12,11 @@ if [ ${#FONTS[@]} -gt 0 ]; then
 
     echo "Installation of nerd-fonts started"
 
+    rm -rf "$DIR_PRINCIPAL"
+
     for font in "${FONTS[@]}"; do
 
         font="$(echo "$font" | sed -e 's|^[[:blank:]]||g' | tr -d '\n')"
-        
-        rm -rf "$DIR_PRINCIPAL/$font"
         
         bash "$FONTS_MODULE_SCRIPTS_DIR"/download.sh "$font" "$COMPACT_FORMAT" "$URL$font.$COMPACT_FORMAT" "$DIR_PRINCIPAL/$font"
 
