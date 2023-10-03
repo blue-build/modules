d35859c669b1b84349c0e2245d41e55cd9b1db51
nhubao <baoprokute1234@gmail.com>
2023-10-03 19:26:40 +0700

    flatten fonts in /usr/share/fonts for distrobox

--- modules/fonts/sources/google-fonts.sh
+++ modules/fonts/sources/google-fonts.sh
@@ -3,7 +3,7 @@ set -oue pipefail
 
 mapfile -t FONTS <<< "$@"
 URL="https://fonts.google.com/download?family="
-DIR_PRINCIPAL=/usr/share/fonts/google-fonts
+DIR_PRINCIPAL=/usr/share/fonts
 COMPACT_FORMAT="zip"
 
 # To download google-fonts it is necessary to enter the name of the font replacing the spaces with '%20'. See the 3rd parameter of the download script.
@@ -18,7 +18,7 @@ if [ ${#FONTS[@]} -gt 0 ]; then
 
         font="$(echo "$font" | sed -e 's|^[[:blank:]]||g' | tr -d '\n')"
 
-        bash "$(dirname "$0")"/../download.sh "$font" "$COMPACT_FORMAT" "$URL${font// /%20}" "$DIR_PRINCIPAL/$font" 
+        bash "$(dirname "$0")"/../download.sh "$font" "$COMPACT_FORMAT" "$URL${font// /%20}" "$DIR_PRINCIPAL/google-fonts-$font" 
 
     done
 
--- modules/fonts/sources/nerd-fonts.sh
+++ modules/fonts/sources/nerd-fonts.sh
@@ -3,7 +3,7 @@ set -oue pipefail
 
 mapfile -t FONTS <<< "$@"
 URL="https://github.com/ryanoasis/nerd-fonts/releases/latest/download/"
-DIR_PRINCIPAL=/usr/share/fonts/nerd-fonts
+DIR_PRINCIPAL=/usr/share/fonts
 COMPACT_FORMAT="zip"
 
 # To download nerd-fonts you need to enter the name of the font and the format you want. See the 3rd parameter of the download script.
@@ -18,7 +18,7 @@ if [ ${#FONTS[@]} -gt 0 ]; then
 
         font="$(echo "$font" | sed -e 's|^[[:blank:]]||g' | tr -d '\n')"
         
-        bash "$(dirname "$0")"/../download.sh "$font" "$COMPACT_FORMAT" "$URL$font.$COMPACT_FORMAT" "$DIR_PRINCIPAL/$font"
+        bash "$(dirname "$0")"/../download.sh "$font" "$COMPACT_FORMAT" "$URL$font.$COMPACT_FORMAT" "$DIR_PRINCIPAL/nerd-fonts-$font"
 
     done
 
