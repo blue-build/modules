4dc9f43e3d7afcc330b0ca9dc1ad30cfa609a53f
Lucas Ribeiro <lucasvsribeiro@outlook.com>
2023-10-13 10:29:57 -0300

    chore: dirs

--- modules/flatpak-sync/flatpak-sync.sh
+++ modules/flatpak-sync/flatpak-sync.sh
@@ -2,12 +2,14 @@
 set -oue pipefail
 
 export BLING_FILES_DIRECTORY="/tmp/bling/files"
+SYSTEMD_USER_JOBS_DIR="/usr/lib/systemd/user/"
+
+mkdir -p $SYSTEMD_USER_JOBS_DIR
 
 chmod +x "$BLING_FILES_DIRECTORY"/usr/bin/flatpaksync/*
 
 cp -r "$BLING_FILES_DIRECTORY"/usr/bin/flatpaksync/* /usr/bin/
 
-cp -r "$BLING_FILES_DIRECTORY"/usr/lib/systemd/user/flatpaksync.service /usr/lib/systemd/user/flatpaksync.service
-cp -r "$BLING_FILES_DIRECTORY"/usr/lib/systemd/user/flatpaksync.path /usr/lib/systemd/user/flatpaksync.path
+cp -r "$BLING_FILES_DIRECTORY"/usr/lib/systemd/user/flatpaksync/* $SYSTEMD_USER_JOBS_DIR
 
 systemctl enable --global flatpaksync.path
