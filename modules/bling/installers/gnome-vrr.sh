dc60cfaa427b87b0fb3c47869dad62016f40878c
xyny <60004820+xynydev@users.noreply.github.com>
2023-12-31 10:44:09 +0000

    fix(gnome-vrr): use correct version variable (#94)

--- modules/bling/installers/gnome-vrr.sh
+++ modules/bling/installers/gnome-vrr.sh
@@ -3,7 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -oue pipefail
 
-wget -O "/etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo" "https://copr.fedorainfracloud.org/coprs/kylegospo/gnome-vrr/repo/fedora-${FEDORA_MAJOR_VERSION}/kylegospo-gnome-vrr-fedora-${FEDORA_MAJOR_VERSION}.repo"
+wget -O "/etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo" "https://copr.fedorainfracloud.org/coprs/kylegospo/gnome-vrr/repo/fedora-${OS_VERSION}/kylegospo-gnome-vrr-fedora-${OS_VERSION}.repo"
 
 rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem xorg-x11-server-Xwayland
 
