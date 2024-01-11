9ff238f5dfddbeca45495b1d345cad503179edb7
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-11 08:55:56 +0100

    There is no need to fetch main-nvidia build for now

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -2,7 +2,7 @@
 set -oue pipefail
 
 get_yaml_array INSTALL '.install[]' "$1"
-IMAGE_NVIDIA=$(echo "${BASE_IMAGE}" | grep -o "main-nvidia" || echo "${BASE_IMAGE}" | grep -o "asus-nvidia" || echo "${BASE_IMAGE}" | grep -o "surface-nvidia") 
+IMAGE_NVIDIA=$(echo "${BASE_IMAGE}" | grep -o "asus-nvidia" || echo "${BASE_IMAGE}" | grep -o "surface-nvidia") 
 IMAGE_DEVICES=$(echo "${BASE_IMAGE}" | sed 's/asus-nvidia//' | grep -o "asus" || echo "${BASE_IMAGE}" | sed 's/surface-nvidia//' | grep -o "surface")
 
 INSTALL_PATH=("${INSTALL[@]/#/\/tmp/rpms/kmods/*}")
