4662a9ae1ca3bfa99afe93ac61c9efcc3661267a
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-12 18:18:42 +0100

    Install kernel-devel-matched for all builds

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-BASED_IMAGE=$(echo "${BASE_IMAGE}")
-
 get_yaml_array INSTALL '.install[]' "$1"
 
 INSTALL_PATH=("${INSTALL[@]/#/\/tmp/rpms/kmods/*}")
@@ -12,9 +10,5 @@ INSTALL_STR=$(echo "${INSTALL_PATH[*]}" | tr -d '\n')
 if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
-  if [[ "$BASED_IMAGE" =~ asus ]] || [[ "$BASED_IMAGE" =~ surface ]]; then
-    rpm-ostree install kernel-tools $INSTALL_STR
-    else
     rpm-ostree install kernel-devel-matched $INSTALL_STR
-  fi  
-fi
+fi    
