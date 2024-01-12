43a664064538cdffb7acdbc8346f43d66b7c2272
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-12 17:02:23 +0100

    Use simpler =~ for conditioning instead of grep & sed
    This finally fixes akmods module

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
+BASED_IMAGE=$(echo "${BASE_IMAGE}")
+
 get_yaml_array INSTALL '.install[]' "$1"
-IMAGE_NVIDIA=$(echo "${BASE_IMAGE}" | grep -o "asus-nvidia" || echo "${BASE_IMAGE}" | grep -o "surface-nvidia") 
-IMAGE_DEVICES=$(echo "${BASE_IMAGE}" | sed 's/asus-nvidia//' | grep -o "asus" || echo "${BASE_IMAGE}" | sed 's/surface-nvidia//' | grep -o "surface")
 
 INSTALL_PATH=("${INSTALL[@]/#/\/tmp/rpms/kmods/*}")
 INSTALL_PATH=("${INSTALL_PATH[@]/%/*.rpm}")
@@ -12,10 +12,9 @@ INSTALL_STR=$(echo "${INSTALL_PATH[*]}" | tr -d '\n')
 if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
-
-  if [[ "$IMAGE_DEVICES" == asus ]] || [ "$IMAGE_NVIDIA" == asus-nvidia ] || [ "$IMAGE_DEVICES" == surface ] || [ "$IMAGE_NVIDIA" == surface-nvidia ]; then
-    rpm-ostree install kernel-tools "$INSTALL_STR"
+  if [[ "$BASED_IMAGE" =~ asus ]] || [[ "$BASED_IMAGE" =~ surface ]]; then
+    rpm-ostree install kernel-tools $INSTALL_STR
     else
-    rpm-ostree install kernel-devel-matched "$INSTALL_STR"
+    rpm-ostree install kernel-devel-matched $INSTALL_STR
   fi  
 fi
