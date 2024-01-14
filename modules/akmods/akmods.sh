ed83ff5ec2bb89858d75ea8235865276e394b618
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-14 23:54:02 +0100

    fix(akmods): Failure when installing akmods from multimedia repo

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -oue pipefail
 
-BASED_IMAGE=$(echo "${BASE_IMAGE}")
+function ENABLE_MULTIMEDIA_REPO {
+  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo && sed -i "0,/enabled/ s@enabled=0@enabled=1@g" /etc/yum.repos.d/negativo17-fedora-multimedia.repo;
+}
+
+function DISABLE_MULTIMEDIA_REPO {
+  sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo;
+}
 
 get_yaml_array INSTALL '.install[]' "$1"
 
@@ -12,9 +18,13 @@ INSTALL_STR=$(echo "${INSTALL_PATH[*]}" | tr -d '\n')
 if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
-  if [[ "$BASED_IMAGE" =~ "surface" ]]; then
+  if [[ "$BASE_IMAGE" =~ "surface" ]]; then
+    ENABLE_MULTIMEDIA_REPO
     rpm-ostree install kernel-surface-devel-matched $INSTALL_STR
-  else  
+    DISABLE_MULTIMEDIA_REPO
+  else
+    ENABLE_MULTIMEDIA_REPO
     rpm-ostree install kernel-devel-matched $INSTALL_STR
+    DISABLE_MULTIMEDIA_REPO
   fi  
 fi    
