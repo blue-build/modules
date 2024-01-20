fe9cd68c6aff2209051a8387e7384ce7cb463f92
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-20 17:38:51 +0100

    fix(default-flatpaks): Install/uninstall system flatpaks regardless i… (#100)
    * fix(default-flatpaks): Install/uninstall system flatpaks regardless if current user has permissions or not
    
    Fixes: https://github.com/ublue-os/bling/pull/93
    
    To make notify-send working for --system systemd units, I have to thank @gerblesh for his code & idea in ublue-updater.
    
    I tested this in Silverblue & it works.
    However, I would like this to be tested in other DEs like Kinoite too, to see if it's working correctly.
    
    Feel free to improve code if you find some space for that.
    
    @lorduskordus
    
    * Use json parser instead as a more reliable solution on fetching values
    
    * There is no need for echo when parsing json
    
    * Fetch DISPLAY environment variable from printenv
    
    * Make functions for notify-sends, to make script clearer
    
    * Whiteline fixes

--- modules/default-flatpaks/default-flatpaks.sh
+++ modules/default-flatpaks/default-flatpaks.sh
@@ -5,11 +5,9 @@ set -euo pipefail
 
 BLING_DIRECTORY="${BLING_DIRECTORY:-"/tmp/bling"}"
 
-cp -r "$BLING_DIRECTORY"/files/usr/bin/system-flatpak-presetup /usr/bin/system-flatpak-presetup
 cp -r "$BLING_DIRECTORY"/files/usr/bin/system-flatpak-setup /usr/bin/system-flatpak-setup
 cp -r "$BLING_DIRECTORY"/files/usr/bin/user-flatpak-setup /usr/bin/user-flatpak-setup
-cp -r "$BLING_DIRECTORY"/files/usr/lib/systemd/system/system-flatpak-presetup.service /usr/lib/systemd/system/system-flatpak-presetup.service
-cp -r "$BLING_DIRECTORY"/files/usr/lib/systemd/user/system-flatpak-setup.service /usr/lib/systemd/user/system-flatpak-setup.service
+cp -r "$BLING_DIRECTORY"/files/usr/lib/systemd/system/system-flatpak-setup.service /usr/lib/systemd/system/system-flatpak-setup.service
 cp -r "$BLING_DIRECTORY"/files/usr/lib/systemd/user/user-flatpak-setup.service /usr/lib/systemd/user/user-flatpak-setup.service
 
 configure_flatpak_repo () {
@@ -106,8 +104,7 @@ configure_lists () {
 
 echo "Enabling flatpaks module"
 mkdir -p /usr/etc/flatpak/{system,user}
-systemctl enable -f system-flatpak-presetup.service
-systemctl enable -f --global system-flatpak-setup.service
+systemctl enable -f system-flatpak-setup.service
 systemctl enable -f --global user-flatpak-setup.service
 
 # Check that `system` is present before configuring
