8619054732d9a063b94c74b979ca1d04e6267c38
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-24 10:35:57 +0100

    fix(akmods): Xone installs conflicted "akmod-xone" package
    This sets higher priority for `akmods repo` compared to `multimedia repo`, which should fix potential issues in the future for other akmods too.
    
    Thanks to the debugging done here:
    https://github.com/ublue-os/akmods/pull/116

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -2,11 +2,19 @@
 set -euo pipefail
 
 function ENABLE_MULTIMEDIA_REPO {
-  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo && sed -i "0,/enabled/ s@enabled=0@enabled=1@g" /etc/yum.repos.d/negativo17-fedora-multimedia.repo;
+  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo && sed -i "0,/enabled/ s@enabled=0@enabled=1@g" /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 }
 
 function DISABLE_MULTIMEDIA_REPO {
-  sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo;
+  sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+}
+
+function SET_HIGHER_PRIORITY_AKMODS_REPO {
+echo "priority=90" >> /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+}
+
+function REVERT_PRIORITY_TO_DEFAULT_AKMODS_REPO {
+sed -i 's/priority=90//' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 }
 
 get_yaml_array INSTALL '.install[]' "$1"
@@ -19,12 +27,16 @@ if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
   if [[ "$BASE_IMAGE" =~ "surface" ]]; then
+    SET_HIGHER_PRIORITY_AKMODS_REPO
     ENABLE_MULTIMEDIA_REPO
     rpm-ostree install kernel-surface-devel-matched $INSTALL_STR
     DISABLE_MULTIMEDIA_REPO
+    REVERT_PRIORITY_TO_DEFAULT_AKMODS_REPO
   else
+    SET_HIGHER_PRIORITY_AKMODS_REPO
     ENABLE_MULTIMEDIA_REPO
     rpm-ostree install kernel-devel-matched $INSTALL_STR
     DISABLE_MULTIMEDIA_REPO
+    REVERT_PRIORITY_TO_DEFAULT_AKMODS_REPO
   fi  
 fi    
