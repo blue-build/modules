8b8fcd3144ac0fd6af52ecbda944ecb60e008a9f
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-01-24 11:45:05 +0100

    fix(akmods): Xone installs conflicted "akmod-xone" package (#102)
    * fix(akmods): Xone installs conflicted "akmod-xone" package
    
    This sets higher priority for `akmods repo` compared to `multimedia repo`, which should fix potential issues in the future for other akmods too.
    
    Thanks to the debugging done here:
    https://github.com/ublue-os/akmods/pull/116
    
    * fix(akmods): There is no need to revert to default, as it doesn't cause issues
    
    On the other side, enabled multimedia repo afterwards causes issues.
    
    * chore(akmods): Format function spacing same as other 2 functions
    
    * chore(akmods): Make ENABLE_MULTIMEDIA_REPO more readable

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -2,11 +2,16 @@
 set -euo pipefail
 
 function ENABLE_MULTIMEDIA_REPO {
-  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo && sed -i "0,/enabled/ s@enabled=0@enabled=1@g" /etc/yum.repos.d/negativo17-fedora-multimedia.repo;
+  sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
+  sed -i "0,/enabled/ s@enabled=0@enabled=1@g" /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 }
 
 function DISABLE_MULTIMEDIA_REPO {
-  sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo;
+  sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
+}
+
+function SET_HIGHER_PRIORITY_AKMODS_REPO {
+  echo "priority=90" >> /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 }
 
 get_yaml_array INSTALL '.install[]' "$1"
@@ -19,10 +24,12 @@ if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
   if [[ "$BASE_IMAGE" =~ "surface" ]]; then
+    SET_HIGHER_PRIORITY_AKMODS_REPO
     ENABLE_MULTIMEDIA_REPO
     rpm-ostree install kernel-surface-devel-matched $INSTALL_STR
     DISABLE_MULTIMEDIA_REPO
   else
+    SET_HIGHER_PRIORITY_AKMODS_REPO
     ENABLE_MULTIMEDIA_REPO
     rpm-ostree install kernel-devel-matched $INSTALL_STR
     DISABLE_MULTIMEDIA_REPO
