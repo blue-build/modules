e1e6ced3edfc15c74d6f04ed59f3c004b2aecf1f
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-23 11:19:46 +0100

    fix(akmods): Supergfxctl extension & Sway environment workaround

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -15,11 +15,11 @@ SET_HIGHER_PRIORITY_AKMODS_REPO() {
 }
 
 INSTALL_AKMODS() {
-    rpm-ostree install kernel-devel-matched $INSTALL_STR
+    rpm-ostree install kernel-devel-matched "${INSTALL_STR}"
 }
 
 INSTALL_SURFACE_AKMODS() {
-    rpm-ostree install kernel-surface-devel-matched $INSTALL_STR
+    rpm-ostree install kernel-surface-devel-matched "${INSTALL_STR}"
 }
 
 # Credits: Universal Blue "-nvidia" repo, install.sh & post-install.sh
@@ -34,14 +34,13 @@ INSTALL_NVIDIA_DRIVER() {
     rpm-ostree install \
     /tmp/rpms/ublue-os/ublue-os-nvidia-addons-*.rpm
     source /tmp/rpms/kmods/nvidia-vars."${NVIDIA_VERSION}"
-    ########## This part needs to be figured out still ##########
-    #if [[ "${IMAGE_NAME}" == "kinoite" ]]; then
-      #VARIANT_PKGS="supergfxctl-plasmoid"
-    #elif [[ "${IMAGE_NAME}" == "silverblue" ]]; then
-      #VARIANT_PKGS="gnome-shell-extension-supergfxctl-gex"
-    #else
-      #VARIANT_PKGS=""
-    #fi
+    if [[ "${OS_VARIANT}" == "kinoite" ]]; then
+      VARIANT_PKGS="supergfxctl-plasmoid"
+    elif [[ "${OS_VARIANT}" == "silverblue" ]]; then
+      VARIANT_PKGS="gnome-shell-extension-supergfxctl-gex"
+    else
+      VARIANT_PKGS=""
+    fi
     rpm-ostree install \
     xorg-x11-drv-"${NVIDIA_PACKAGE_NAME}"-{,cuda-,devel-,kmodsrc-,power-}"${NVIDIA_FULL_VERSION}" \
     xorg-x11-drv-"${NVIDIA_PACKAGE_NAME}"-libs.i686 \
@@ -52,25 +51,26 @@ INSTALL_NVIDIA_DRIVER() {
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/{eyecantcu-supergfxctl,nvidia-container-toolkit}.repo
     systemctl enable ublue-nvctk-cdi.service
     semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
-    ########## This part needs to be figured out still ##########
-    #if [[ "${IMAGE_NAME}" == "sericea" ]]; then
-      #mv /etc/sway/environment{,.orig}
-      #install -Dm644 /usr/share/ublue-os/etc/sway/environment /etc/sway/environment
-    #fi
+    if [[ "${OS_VARIANT}" == "sericea" ]]; then
+      mv /etc/sway/environment{,.orig}
+      install -Dm644 /usr/share/ublue-os/etc/sway/environment /etc/sway/environment
+    fi
 }
 
-get_yaml_array INSTALL '.install[]' "$1"
+OS_VARIANT=$(grep -Po 'VARIANT_ID=\K\w+' /usr/lib/os-release)
 NVIDIA_VERSION=$(echo "$1" | yq -I=0 ".nvidia-version")
 NVIDIA=$(rpm -qa --queryformat '%{NAME}\n' | grep "nvidia")
 SURFACE=$(rpm -qa --queryformat '%{NAME}\n' | awk '$0 == "kernel-surface"')
+
+get_yaml_array INSTALL '.install[]' "$1"
 INSTALL_PATH=("${INSTALL[@]/#/\/tmp/rpms/kmods/*}")
 INSTALL_PATH=("${INSTALL_PATH[@]/%/*.rpm}")
 INSTALL_STR=$(echo "${INSTALL_PATH[*]}" | tr -d '\n')
 
 # Errors out if unsupported Nvidia driver version is specified in recipe.
 # Needs to be updated whenever new Nvidia driver releases for check to work properly
-if [[ -n "$NVIDIA_VERSION" ]]; then
-  if [[ ! "$NVIDIA_VERSION" == "545" ]] && [[ ! "$NVIDIA_VERSION" == "470" ]]; then
+if [[ -n "${NVIDIA_VERSION}" ]]; then
+  if [[ ! "${NVIDIA_VERSION}" == "545" ]] && [[ ! "${NVIDIA_VERSION}" == "470" ]]; then
     echo "You provided unsupported Nvidia akmod version in nvidia-version recipe entry, only v545 & v470 are supported."
     exit 1
   fi
@@ -79,24 +79,24 @@ fi
 if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
-  if [[ -n "$SURFACE" ]] && [[ -z "$NVIDIA" ]] && [[ -n "$NVIDIA_VERSION" ]]; then
+  if [[ -n "${SURFACE}" ]] && [[ -z "${NVIDIA}" ]] && [[ -n "${NVIDIA_VERSION}" ]]; then
     SET_HIGHER_PRIORITY_AKMODS_REPO
     ENABLE_MULTIMEDIA_REPO
     INSTALL_SURFACE_AKMODS
     DISABLE_MULTIMEDIA_REPO
     INSTALL_NVIDIA_DRIVER
-  elif [[ -z "$SURFACE" ]] && [[ -z "$NVIDIA" ]] && [[ -n "$NVIDIA_VERSION" ]]; then
+  elif [[ -z "${SURFACE}" ]] && [[ -z "${NVIDIA}" ]] && [[ -n "${NVIDIA_VERSION}" ]]; then
     SET_HIGHER_PRIORITY_AKMODS_REPO
     ENABLE_MULTIMEDIA_REPO
     INSTALL_AKMODS
     DISABLE_MULTIMEDIA_REPO    
     INSTALL_NVIDIA_DRIVER
-  elif [[ -n "$SURFACE" ]] && [[ -z "$NVIDIA_VERSION" ]]; then
+  elif [[ -n "${SURFACE}" ]] && [[ -z "${NVIDIA_VERSION}" ]]; then
     SET_HIGHER_PRIORITY_AKMODS_REPO
     ENABLE_MULTIMEDIA_REPO
     INSTALL_SURFACE_AKMODS
     DISABLE_MULTIMEDIA_REPO
-  elif [[ -z "$SURFACE" ]] && [[ -z "$NVIDIA_VERSION" ]]; then
+  elif [[ -z "${SURFACE}" ]] && [[ -z "${NVIDIA_VERSION}" ]]; then
     SET_HIGHER_PRIORITY_AKMODS_REPO
     ENABLE_MULTIMEDIA_REPO
     INSTALL_AKMODS
