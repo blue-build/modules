2157b7eb5ed552628636f7b13b09270816292c7a
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-24 19:03:10 +0100

    fix(fonts): Module failing with legacy templates (#141)
    Issue described here:
    https://github.com/blue-build/github-action/issues/16#issuecomment-1961862342

--- modules/fonts/fonts.sh
+++ modules/fonts/fonts.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Workaround for fonts module failing on legacy templates (with build.sh)
+get_yaml_array() {
+    # creates array $1 with content at key $2 from $3
+    readarray "$1" < <(echo "$3" | yq -I=0 "$2")
+}
+
 export FONTS_MODULE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 for source in "$FONTS_MODULE_DIR"/sources/*.sh; do
