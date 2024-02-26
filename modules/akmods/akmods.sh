af7a6de99c192ab100344d62ac14b4a836c95cef
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-02-26 19:48:41 +0100

    fix(akmods): Fix OS_VARIANT check for Sway Atomic.
    Sway is going to be renamed from "Sericea" to "Sway Atomic", so update this check to reflect that change.
    
    Related PR:
    https://src.fedoraproject.org/rpms/fedora-release/pull-request/314

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -51,7 +51,7 @@ INSTALL_NVIDIA_DRIVER() {
     sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/{eyecantcu-supergfxctl,nvidia-container-toolkit}.repo
     systemctl enable ublue-nvctk-cdi.service
     semodule --verbose --install /usr/share/selinux/packages/nvidia-container.pp
-    if [[ "${OS_VARIANT}" == "sericea" ]]; then
+    if [[ "${OS_VARIANT}" == "sway-atomic" ]]; then
       mv /etc/sway/environment{,.orig}
       install -Dm644 /usr/share/ublue-os/etc/sway/environment /etc/sway/environment
     fi
