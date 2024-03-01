ce8714f371c1fd96a64c7a2ceb1b2c0d69569071
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-03-01 15:02:04 +0100

    feat(systemd): Add support for zram-generator & {system,user}.conf.d inclusions

--- modules/systemd/README.md
+++ modules/systemd/README.md
@@ -11,4 +11,23 @@ Those units are then copied into these folders (depending on unit base):
 `/usr/lib/systemd/system`  
 `/usr/lib/systemd/user`
 
-Supported management operations are [enabling](https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#enable%20UNIT%E2%80%A6), [disabling](https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#disable%20UNIT%E2%80%A6), [masking](https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#mask%20UNIT%E2%80%A6%E2%80%A6) and [unmasking](https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#unmask%20UNIT%E2%80%A6).
+Specific systemd config can be included in these directories, depending on certain usecases:  
+`config/systemd/system.conf.d`  
+`config/systemd/user.conf.d`  
+`config/systemd/zram-generator.conf.d`
+
+Which are then copied into these folders:  
+`/usr/lib/systemd/system.conf.d`  
+`/usr/lib/systemd/user.conf.d`  
+`/usr/lib/systemd/zram-generator.conf.d`
+
+Supported systemd unit management operations are:  
+ [enabling](https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#enable%20UNIT%E2%80%A6)  
+[disabling](https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#disable%20UNIT%E2%80%A6)  
+[masking](https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#mask%20UNIT%E2%80%A6%E2%80%A6  
+[unmasking](https://www.freedesktop.org/software/systemd/man/latest/systemctl.html#unmask%20UNIT%E2%80%A6)
+
+Supported systemd configs are:  
+[system.conf.d & user.conf.d](https://www.freedesktop.org/software/systemd/man/latest/systemd-system.conf.html)  
+[zram-generator.conf.d](https://github.com/systemd/zram-generator/blob/main/zram-generator.conf.example)
+
--- modules/systemd/systemd.sh
+++ modules/systemd/systemd.sh
@@ -3,24 +3,48 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
-# Copy included systemd unit files to system
-SYSTEM_UNIT_INCLUDE="$CONFIG_DIRECTORY"/systemd/system
-USER_UNIT_INCLUDE="$CONFIG_DIRECTORY"/systemd/user
+# Copy included systemd unit files to system. Also supports zram-generator & system/user conf.d
+SYSTEM_UNIT_INCLUDE="${CONFIG_DIRECTORY}/systemd/system"
+USER_UNIT_INCLUDE="${CONFIG_DIRECTORY}/systemd/user"
+ZRAM_GENERATOR_INCLUDE="${CONFIG_DIRECTORY}/systemd/zram-generator.conf.d"
+SYSTEM_CONF_D_INCLUDE="${CONFIG_DIRECTORY}/systemd/system.conf.d"
+USER_CONF_D_INCLUDE="${CONFIG_DIRECTORY}/systemd/user.conf.d"
 SYSTEM_UNIT_DIR="/usr/lib/systemd/system"
 USER_UNIT_DIR="/usr/lib/systemd/user"
+ZRAM_GENERATOR_DIR="/usr/lib/systemd/zram-generator.conf.d"
+SYSTEM_CONF_D_DIR="/usr/lib/systemd/system.conf.d"
+USER_CONF_D_DIR="/usr/lib/systemd/user.conf.d"
 
-if [[ -d "$SYSTEM_UNIT_INCLUDE" ]]; then
-  if [[ -n $(find "$SYSTEM_UNIT_INCLUDE" -type f) ]]; then
+if [[ -d "${SYSTEM_UNIT_INCLUDE}" ]]; then
+  if [[ -n $(find "${SYSTEM_UNIT_INCLUDE}" -type f) ]]; then
     echo "Copying 'system' systemd units to system directory"
-    cp -r "$SYSTEM_UNIT_INCLUDE"/* "$SYSTEM_UNIT_DIR"
+    cp -r "${SYSTEM_UNIT_INCLUDE}/*" "${SYSTEM_UNIT_DIR}"
   fi
 fi
-if [[ -d "$USER_UNIT_INCLUDE" ]]; then
-  if [[ -n $(find "$USER_UNIT_INCLUDE" -type f) ]]; then
+if [[ -d "${USER_UNIT_INCLUDE}" ]]; then
+  if [[ -n $(find "${USER_UNIT_INCLUDE}" -type f) ]]; then
     echo "Copying 'user' systemd units to system directory"  
-    cp -r "$USER_UNIT_INCLUDE"/* "$USER_UNIT_DIR"
+    cp -r "${USER_UNIT_INCLUDE}/*" "${USER_UNIT_DIR}"
   fi  
 fi  
+if [[ -d "${ZRAM_GENERATOR_INCLUDE}" ]]; then
+  if [[ -n $(find "${ZRAM_GENERATOR_INCLUDE}" -type f) ]]; then
+    echo "Copying 'system' systemd units to system directory"
+    cp -r "${ZRAM_GENERATOR_INCLUDE}/*" "${ZRAM_GENERATOR_DIR}"
+  fi
+fi
+if [[ -d "${SYSTEM_CONF_D_INCLUDE}" ]]; then
+  if [[ -n $(find "${SYSTEM_CONF_D_INCLUDE}" -type f) ]]; then
+    echo "Copying 'system' systemd units to system directory"
+    cp -r "${SYSTEM_CONF_D_INCLUDE}/*" "${SYSTEM_CONF_D_DIR}"
+  fi
+fi
+if [[ -d "${USER_CONF_D_INCLUDE}" ]]; then
+  if [[ -n $(find "${USER_CONF_D_INCLUDE}" -type f) ]]; then
+    echo "Copying 'system' systemd units to system directory"
+    cp -r "${USER_CONF_D_INCLUDE}/*" "${USER_CONF_D_DIR}"
+  fi
+fi
 
 # Systemd units configuration (enable, disable, unmask & mask)
 get_yaml_array ENABLED '.system.enabled[]' "$1"
@@ -34,49 +58,41 @@ get_yaml_array USER_MASKED '.user.masked[]' "$1"
 
 if [[ ${#ENABLED[@]} -gt 0 ]]; then
     for unit in "${ENABLED[@]}"; do
-        unit=$(printf "$unit")
-        systemctl -f enable $unit
+        systemctl -f enable "${unit}"
     done
 fi
 if [[ ${#DISABLED[@]} -gt 0 ]]; then
     for unit in "${DISABLED[@]}"; do
-        unit=$(printf "$unit")
-        systemctl disable $unit
+        systemctl disable "${unit}"
     done
 fi
 if [[ ${#UNMASKED[@]} -gt 0 ]]; then
     for unit in "${UNMASKED[@]}"; do
-        unit=$(printf "$unit")
-        systemctl unmask $unit
+        systemctl unmask "${unit}"
     done
 fi
 if [[ ${#MASKED[@]} -gt 0 ]]; then
     for unit in "${MASKED[@]}"; do
-        unit=$(printf "$unit")
-        systemctl mask $unit
+        systemctl mask "${unit}"
     done
 fi
 if [[ ${#USER_ENABLED[@]} -gt 0 ]]; then
     for unit in "${USER_ENABLED[@]}"; do
-        unit=$(printf "$unit")
-        systemctl --global -f enable $unit
+        systemctl --global -f enable "${unit}"
     done
 fi
 if [[ ${#USER_DISABLED[@]} -gt 0 ]]; then
     for unit in "${USER_DISABLED[@]}"; do
-        unit=$(printf "$unit")
-        systemctl --global disable $unit
+        systemctl --global disable "${unit}"
     done
 fi
 if [[ ${#USER_UNMASKED[@]} -gt 0 ]]; then
     for unit in "${USER_UNMASKED[@]}"; do
-        unit=$(printf "$unit")
-        systemctl --global unmask $unit
+        systemctl --global unmask "${unit}"
     done
 fi
 if [[ ${#USER_MASKED[@]} -gt 0 ]]; then
     for unit in "${USER_MASKED[@]}"; do
-        unit=$(printf "$unit")
-        systemctl --global mask $unit
+        systemctl --global mask "${unit}"
     done
 fi
