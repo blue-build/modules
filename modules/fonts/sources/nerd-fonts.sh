3de87f3086d6113dc2a68a3f1be0ef71a480e2ee
xynydev <60004820+xynydev@users.noreply.github.com>
2024-03-29 13:22:22 +0200

    fix: make /tmp/fonts directory before attempting use

--- modules/fonts/sources/nerd-fonts.sh
+++ modules/fonts/sources/nerd-fonts.sh
@@ -8,6 +8,7 @@ DEST="/usr/share/fonts/nerd-fonts"
 echo "Installation of nerd-fonts started"
 rm -rf "$DEST"
 
+mkdir -p /tmp/fonts
 for FONT in "${FONTS[@]}"; do
     mkdir -p "${DEST}/${FONT}"
 
@@ -16,5 +17,6 @@ for FONT in "${FONTS[@]}"; do
     curl "${URL}/${FONT}.tar.xz" -L -o "/tmp/fonts/${FONT}.tar.xz"
     tar -xf "/tmp/fonts/${FONT}.tar.xz" -C "${DEST}/${FONT}"
 done
+rm -rf /tmp/fonts
 
 fc-cache -f "${DEST}"
