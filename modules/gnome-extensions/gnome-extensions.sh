1ed844fd85e304db5bad24a180040572e2e3d138
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-04-09 09:09:43 +0200

    fix: Get rid of `gettext-domain` key, `install` key is already sufficient

--- modules/gnome-extensions/gnome-extensions.sh
+++ modules/gnome-extensions/gnome-extensions.sh
@@ -3,7 +3,7 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
-get_yaml_array GETTEXT_DOMAIN '.install.gettext-domain[]' "$1"
+get_yaml_array GETTEXT_DOMAIN '.install[]' "$1"
 GNOME_VER=$(gnome-shell --version | sed 's/[^0-9]*\([0-9]*\).*/\1/')
 echo "Gnome version: v${GNOME_VER}"
 
