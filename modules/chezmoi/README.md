fb613fc2fa88d1fe90abc5397659b6f92616cd64
exponentactivity <18619090+exponentactivity@users.noreply.github.com>
2024-04-30 18:38:24 +0200

    Feat: Add chezmoi module

---
+++ modules/chezmoi/README.md
@@ -0,0 +1,55 @@
+# `chezmoi`
+
+The `chezmoi` module takes care of installing, initializing and updating you dotfiles.
+Each feature can be enabled or disabled individually.
+
+Installation of the `chezmoi` binary happens at build time and is done by downloading the `amd64` binary from the latest release to `/usr/bin/chezmoi`. 
+This can be disabled by setting `install` to false. (defaults: true)
+
+A systemd user service is installed that will initialize a `chezmoi` repository on chezmoi's default path (`~/.local/share/chezmoi`) for any user when it logs in, or at boot if it has lingering enabled.
+The service will only run if `~/.local/share/chezmoi` does not exist.
+Set `repository` to the URL of your dotfiles repository. (eg. `repository: https://example.org/user/dotfiles`)
+:::note
+The value of `repository` will be passed directly to `chezmoi init --apply ${repository}`.
+See the [`chezmoi init` documenation](https://www.chezmoi.io/reference/commands/init/) for detailed syntax.
+::: 
+Set `disable_init` to `true` if you do not want to install the init service.
+
+:::caution
+If `repository` is not set, and `disable_init` is false the module will fail, due to not being able to initialize the repository.
+:::
+
+Set `enable_all_users` to `false` if you want to install the update and initialization services, but do not want them enabled for all users.
+You can enable them manually instead when the system has been installed:
+
+To enable the services for a single user, run the following command as that user:
+
+```bash
+systemctl enable --user chezmoi-init.service chezmoi-update.timer`
+```
+
+To manually enable the services for all users, run the following command with sudo:
+
+```bash
+sudo systemctl enable --user chesmoi-init.service chezmoi-update.timer
+```
+
+To turn on lingering for a given user, run the following commmand with sudo:
+
+```bash
+sudo loginctl enable-linger <username>`
+```
+
+You can configure the interval between updates of your dotfiles by setting the value of `run_every`.
+The string is passed directly to OnUnitInactiveSec. (default: '1d')
+See [`systemd.time` documenation](https://www.freedesktop.org/software/systemd/man/latest/systemd.time.html) for detailed syntax.
+Examples: '1d' (1 day - default), '6h' (6 hours), '10m' (10 minutes)
+
+Likewise, `wait_after_boot` configures the delay between the system booting and the update service starting.
+This follows the same syntax as `run_every`. (default: '5m')
+
+The installation of the initialization service and the update service can be disabled separately by setting `disable_init` and/or `disable_update` to `true`. (Both default: false)
+
+:::caution
+Note that this will skip the installation of the services completely. If you want them installed but disabled, see `enable_all_users` instead.
+:::
---
+++ modules/chezmoi/chezmoi.sh
@@ -0,0 +1,127 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+
+# `-I=0` makes sure the output isn't indented
+
+# If true, downloads the chezmoi binary from the latest Github release and moves it to /usr/bin/. (default: true)
+INSTALL_CHEZMOI==$(echo "$1" | yq -I=0 ".install") # (boolean)
+INSTALL_CHEZMOI=${INSTALL_CHEZMOI:-true}
+
+# The repository with your chezmoi dotfiles. (default: null)
+DOTFILE_REPOSITORY=$(echo "$1" | yq -I=0 ".repository") # (string)
+
+# If true, chezmoi services will be enabled for all logged in users, and users with lingering enabled. (default: true)
+# If false, chezmoi services will not be enabled for any users, but can be enabled manually, after installation.
+#
+# To enable the services for a single user, run the following command as that user:
+# `systemctl enable --user chezmoi-init.service chezmoi-update.timer`
+#
+# To manually enable the services for all users, run the following command with sudo:
+# `sudo systemctl enable --user chesmoi-init.service chezmoi-update.timer`
+#
+# To turn on lingering for a given user, run the following commmand with sudo:
+# `sudo loginctl enable-linger <username>`
+ENABLE_ALL_USERS=$(echo "$1" | yq -I=0 ".enable_all_users") # (boolean)
+ENABLE_ALL_USERS=${ENABLE_ALL_USERS:-true}
+
+# chezmoi-update.service will run with this interval
+# This string is passed on directly to systemd's OnUnitInactiveSec. Complete syntax is described here:
+# https://www.freedesktop.org/software/systemd/man/latest/systemd.time.html#
+# Examples: '1d' (1 day - default), '6h' (6 hours), '10m' (10 minutes)
+RUN_EVERY=$(echo "$1" | yq -I=0 ".run_every") # (string)
+RUN_EVERY=${RUN_EVERY:-'1d'}
+# chezmoi-update.service will also run this much time after the system has booted.
+# Same syntax as RUN_EVERY (default: '5m')
+WAIT_AFTER_BOOT=$(echo "$1" | yq -I=0 ".wait_after_boot") # (string)
+WAIT_AFTER_BOOT=${WAIT_AFTER_BOOT:-'5m'}
+
+# If true, disables automatic initialization of chezmoi if no dotfile directory is found. (default: false)
+DISABLE_INIT=$(echo "$1" | yq -I=0 ".disable_init") # (boolean)
+DISABLE_INIT=${DISABLE_INIT:-false}
+# If true, disables automatic activation of `chezmoi-update.timer`. (default: false)
+DISABLE_UPDATE=$(echo "$1" | yq -I=0 ".disable_update") # (boolean)
+DISABLE_UPDATE=${DISABLE_UPDATE:-false}
+
+echo "Checking if `repository` is not set and `disable_init` is not true."
+if [ ! -v DOTFILE_REPOSITORY && ! DISABLE_INIT ]; then
+  echo "ERROR: `repository` is not set, but initialization is not disabled."
+  echo "Set a value for `repository` or set `disable_update` to true, if you do not wish to initialize a chezmoi directory using this module"
+  exit 1
+fi
+
+if [ INSTALL_CHEZMOI ]; then
+  echo "Checking if curl is installed and executable at /usr/bin/curl"
+  if [ -x /usr/bin/curl]; then
+    echo "Downloading chezmoi binary from the latest Github release"
+    /usr/bin/curl -Ls https://github.com/twpayne/chezmoi/releases/latest/download/chezmoi-linux-amd64 -o /usr/bin/chezmoi
+    echo "Ensuring chezmoi is executable"
+    /usr/bin/chmod 755 /usr/bin/chezmoi
+  else
+    echo "ERROR: curl could not be found in /usr/bin/."
+    echo "Please make sure curl is installed on the system you are building your image."
+    exit 1
+  fi
+fi
+
+if [ ! DISABLE_INIT ]; then
+  # Write the service to initialize Chezmoi, and insert the repo url in the file
+  echo "Writing init service to user unit directory"
+  cat >> /usr/lib/systemd/user/chezmoi-init.service << EOF
+  [Unit]
+  Description=Initializes Chezmoi if directory is missing
+  # This service will not execute for a user with an existing chezmoi directory
+  ConditionPathExists=!%h/.local/share/chezmoi
+  [Service]
+  ExecStart=/usr/bin/chezmoi init --apply ${DOTFILE_REPOSITORY}
+  Type=oneshot
+
+  [Install]
+  WantedBy=multi-user.target
+  Requires=network-online.target
+  After=network-online.target
+EOF
+fi
+
+if [ ! DISABLE_UPDATE ]; then
+  # Write the service and timer to update chezmoi for all logged in users and users with lingering enabled
+  echo "Writing update service to user unit directory"
+  cat >> /usr/lib/systemd/user/chezmoi-update.service << EOF
+  [Unit]
+  Description=Chezmoi Update
+
+  [Service]
+  ExecStart=/usr/bin/chezmoi update
+  Type=oneshot
+EOF
+
+  echo "Writing update timer to user unit directory"
+  cat >> /usr/lib/systemd/user/chezmoi-update.timer << EOF
+  [Unit]
+  Description=Timer for Chezmoi Update
+  # This service will only execute for a user with an existing chezmoi directory
+  ConditionPathExists=%h/.local/share/chezmoi
+
+  [Timer]
+  OnBootSec=${WAIT_AFTER_BOOT}
+  OnUnitInactiveSec=${RUN_EVERY}
+
+  [Install]
+  WantedBy=timers.target
+EOF
+fi
+
+# Enable services
+if [ ENABLE_ALL_USERS && ! DISABLE_INIT && ! DISABLE_UPDATE]; then
+  echo "Enabling init timer and update service"
+  systemctl --user enable --now chezmoi-init.service chezmoi-update.timer
+elif [ ENABLE_ALL_USERS && DISABLE_INIT && ! DISABLE_UPDATE]; then
+  echo "Enabling update timer and disabling init service"
+  systemctl --user enable --now chezmoi-update.timer
+  systemctl --user disable --now chesmoi-init.service
+elif [ ENABLE_ALL_USERS && ! DISABLE_INIT && DISABLE_UPDATE]; then
+  echo "Enabling init service and disabling update service"
+  systemctl --user enable --now chezmoi-init.service
+  systemctl --user disable --now chezmoi-update.service
+fi
