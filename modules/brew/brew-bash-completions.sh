fcc2e4e0b9b7659e8dd5791663893f81b2e570ef
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-05-31 19:20:43 +0200

    chore(brew): Make bash shell completion spacing better

--- modules/brew/brew-bash-completions.sh
+++ modules/brew/brew-bash-completions.sh
@@ -3,23 +3,22 @@
 
 # Check for interactive bash and that we haven't already been sourced.
 if [[ "${BASH_VERSION-}" != "" ]] && [[ "${PS1-}" != "" ]] && [[ "${BREW_BASH_COMPLETION-}" == "" ]]; then
-    # Check for recent enough version of bash.
-    if [[ "${BASH_VERSINFO[0]}" -gt 4 ]] ||
-        [[ "${BASH_VERSINFO[0]}" -eq 4 && "${BASH_VERSINFO[1]}" -ge 2 ]]; then
-        if [[ -w /home/linuxbrew/.linuxbrew ]]; then
-            if [[ ! -L /home/linuxbrew/.linuxbrew/etc/bash_completion.d/brew ]]; then
-                /home/linuxbrew/.linuxbrew/bin/brew completions link > /dev/null
-            fi
-        fi
-        if [[ -d /home/linuxbrew/.linuxbrew/etc/bash_completion.d ]]; then
-            for rc in /home/linuxbrew/.linuxbrew/etc/bash_completion.d/*; do
-                if [[ -r "${rc}" ]]; then
-                    . "${rc}"
-                fi
-            done
-            unset rc
-        fi
+  # Check for recent enough version of bash.
+  if [[ "${BASH_VERSINFO[0]}" -gt 4 ]] || [[ "${BASH_VERSINFO[0]}" -eq 4 && "${BASH_VERSINFO[1]}" -ge 2 ]]; then
+    if [[ -w /home/linuxbrew/.linuxbrew ]]; then
+      if [[ ! -L /home/linuxbrew/.linuxbrew/etc/bash_completion.d/brew ]]; then
+        /home/linuxbrew/.linuxbrew/bin/brew completions link > /dev/null
+      fi
     fi
-    BREW_BASH_COMPLETION=1
-    export BREW_BASH_COMPLETION
+    if [[ -d /home/linuxbrew/.linuxbrew/etc/bash_completion.d ]]; then
+      for rc in /home/linuxbrew/.linuxbrew/etc/bash_completion.d/*; do
+          if [[ -r "${rc}" ]]; then
+            . "${rc}"
+          fi
+      done
+      unset rc
+    fi
+  fi
+  BREW_BASH_COMPLETION=1
+  export BREW_BASH_COMPLETION
 fi
