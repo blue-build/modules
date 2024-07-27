368f28ea0b0d545ca197f6a9463d25879bffa352
xyny <git@xyny.anonaddy.me>
2024-07-27 17:49:46 +0300

    fix(bling): gnome-vrr version check and copr fix

--- modules/bling/installers/gnome-vrr.sh
+++ modules/bling/installers/gnome-vrr.sh
@@ -3,8 +3,14 @@
 # Tell build process to exit if there are any errors.
 set -euo pipefail
 
-wget -O "/etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo" "https://copr.fedorainfracloud.org/coprs/kylegospo/gnome-vrr/repo/fedora-${OS_VERSION}/kylegospo-gnome-vrr-fedora-${OS_VERSION}.repo"
+if [[ $(gnome-shell --version) =~ "46" ]]; then
+    echo "Attention: VRR is supported out of the box on GNOME 46, no need to install it separately. Use the following command to enable VRR on GNOME 46:"
+    echo "gsettings set org.gnome.mutter experimental-features \"['variable-refresh-rate']\""
+    exit 1
+fi
 
-rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:kylegospo:gnome-vrr mutter mutter-common gnome-control-center gnome-control-center-filesystem xorg-x11-server-Xwayland
+wget -O "/etc/yum.repos.d/ublue-os-staging.repo" "https://copr.fedorainfracloud.org/coprs/ublue-os/staging/repo/fedora-${OS_VERSION}/ublue-os-staging-fedora-${OS_VERSION}.repo"
 
-rm -f /etc/yum.repos.d/_copr_kylegospo-gnome-vrr.repo
+rpm-ostree override replace --experimental --from repo=copr:copr.fedorainfracloud.org:ublue-os:staging mutter mutter-common gnome-control-center gnome-control-center-filesystem xorg-x11-server-Xwayland
+
+rm -f /etc/yum.repos.d/ublue-os-staging.repo
