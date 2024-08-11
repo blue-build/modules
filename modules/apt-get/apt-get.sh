a3e539890eb3cb4b7041aa3544407538ef47d8f7
xyny <git@xyny.anonaddy.me>
2024-08-11 16:56:42 +0300

    feat(apt-get): support removing packages

--- modules/apt-get/apt-get.sh
+++ modules/apt-get/apt-get.sh
@@ -4,6 +4,9 @@
 set -euo pipefail
 
 get_yaml_array INSTALL_PKGS '.install[]' "$1"
-
 apt-get install -y "${INSTALL_PKGS[@]}"
+
+get_yaml_array REMOVE_PKGS '.remove[]' "$1"
+apt-get remove -y "${REMOVE_PKGS[@]}"
+
 apt-get clean
