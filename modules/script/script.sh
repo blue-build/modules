c80bbcfb018e0d2fc8cee4f23ab85d650fec6709
xyny <60004820+xynydev@users.noreply.github.com>
2024-09-19 16:15:33 +0300

    fix(script): only cd into scripts directory if scripts are defined (#335)

--- modules/script/script.sh
+++ modules/script/script.sh
@@ -6,14 +6,18 @@ set -euo pipefail
 get_yaml_array SCRIPTS '.scripts[]' "$1"
 get_yaml_array SNIPPETS '.snippets[]' "$1"
 
-cd "$CONFIG_DIRECTORY/scripts"
-# Make every script executable
-find "$PWD" -type f -exec chmod +x {} \;
-for SCRIPT in "${SCRIPTS[@]}"; do
-    echo "Running script $SCRIPT"
-    "$PWD/$SCRIPT"
-done
+# shellcheck disable=SC2153
+if [[ ${#SCRIPTS[@]} -gt 0  ]]; then
+    cd "$CONFIG_DIRECTORY/scripts"
+    # Make every script executable
+    find "$PWD" -type f -exec chmod +x {} \;
+    for SCRIPT in "${SCRIPTS[@]}"; do
+        echo "Running script $SCRIPT"
+        "$PWD/$SCRIPT"
+    done
+fi
 
+# shellcheck disable=SC2153
 for SNIPPET in "${SNIPPETS[@]}"; do
     echo "Running snippet $SNIPPET"
     bash -c "$SNIPPET"
