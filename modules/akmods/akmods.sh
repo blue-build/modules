58946ae1732a414783dd566b188299666c0ebd94
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-10-01 14:26:53 +0200

    chore(akmods): Utilize `readonly` for static variables

--- modules/akmods/akmods.sh
+++ modules/akmods/akmods.sh
@@ -1,30 +1,33 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-function ENABLE_MULTIMEDIA_REPO {
+ENABLE_MULTIMEDIA_REPO() {
   sed -i 's@enabled=0@enabled=1@g' /etc/yum.repos.d/_copr_ublue-os-akmods.repo
   sed -i "0,/enabled/ s@enabled=0@enabled=1@g" /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 }
+readonly -f ENABLE_MULTIMEDIA_REPO
 
-function DISABLE_MULTIMEDIA_REPO {
+DISABLE_MULTIMEDIA_REPO() {
   sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/negativo17-fedora-multimedia.repo
 }
+readonly -f DISABLE_MULTIMEDIA_REPO
 
-function SET_HIGHER_PRIORITY_AKMODS_REPO {
+SET_HIGHER_PRIORITY_AKMODS_REPO() {
   echo "priority=90" >> /etc/yum.repos.d/_copr_ublue-os-akmods.repo
 }
+readonly -f SET_HIGHER_PRIORITY_AKMODS_REPO
 
 get_yaml_array INSTALL '.install[]' "$1"
 
 INSTALL_PATH=("${INSTALL[@]/#/\/tmp/rpms/kmods/*}")
-INSTALL_PATH=("${INSTALL_PATH[@]/%/*.rpm}")
-INSTALL_STR=$(echo "${INSTALL_PATH[*]}" | tr -d '\n')
+readonly INSTALL_PATH=("${INSTALL_PATH[@]/%/*.rpm}")
+readonly INSTALL_STR=$(echo "${INSTALL_PATH[*]}" | tr -d '\n')
 
 if [[ ${#INSTALL[@]} -gt 0 ]]; then
   echo "Installing akmods"
   echo "Installing: $(echo "${INSTALL[*]}" | tr -d '\n')"
   SET_HIGHER_PRIORITY_AKMODS_REPO
   ENABLE_MULTIMEDIA_REPO
-  rpm-ostree install $INSTALL_STR
+  rpm-ostree install ${INSTALL_STR}
   DISABLE_MULTIMEDIA_REPO
 fi    
