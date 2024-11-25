387a29e127a8442916b8ea133e316e411abb1af4
xyny <60004820+xynydev@users.noreply.github.com>
2024-11-25 10:26:10 +0200

    feat(bling): rpmfusion & negativo17 repo helpers (#363)
    * feat(bling): rpmfusion & negativo17 repo helpers
    
    * chore: Make adding negativo repo more robust
    
    * chore: Make sure that priority is set when negativo repo is installed
    
    * chore: Improve RPMFusion repo install robustness
    
    * chore: Fix some indendation
    
    * chore: Fix some indetation pt. 2
    
    * chore: Install all RPMFusion repos in cases when only free or non-free repo is installed
    
    * chore: Install all RPMFusion repos in cases when only free or non-free repo is installed pt. 2
    
    ---------
    
    Co-authored-by: fiftydinar <65243233+fiftydinar@users.noreply.github.com>

---
+++ modules/bling/installers/rpmfusion.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+# Tell build process to exit if there are any errors.
+set -euo pipefail
+
+NEGATIVO_REPO_FILE="$(awk -F'=' '$1 == "name" && $2 == "negativo17 - Multimedia" {print FILENAME}' /etc/yum.repos.d/*)"
+
+# Check if rpmfusion is already installed before running
+if ! rpm -q rpmfusion-free-release &>/dev/null || ! rpm -q rpmfusion-nonfree-release &>/dev/null; then
+  echo "Running RPMFusion repo install..."
+  if ! rpm -q rpmfusion-free-release &>/dev/null && ! rpm -q rpmfusion-nonfree-release &>/dev/null; then
+    rpm-ostree install \
+      "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm" \
+      "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm"
+  elif ! rpm -q rpmfusion-free-release &>/dev/null; then
+    rpm-ostree install \
+      "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm"
+  elif ! rpm -q rpmfusion-nonfree-release &>/dev/null; then
+    rpm-ostree install \
+      "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm"
+  fi
+  # check if negativo17 repo is installed
+  if [[ -n "${NEGATIVO_REPO_FILE}" ]]; then
+    echo "Making sure that Negativo17 repo is disabled"
+    sed -i 's@enabled=1@enabled=0@g' "${NEGATIVO_REPO_FILE}"
+  fi
+else
+  echo "RPMFusion repo is already installed"
+fi
