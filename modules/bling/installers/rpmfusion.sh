2eae564a3b9d8cc026c6f3f115229f23273e259a
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-11-25 09:23:47 +0100

    chore: Install all RPMFusion repos in cases when only free or non-free repo is installed pt. 2

--- modules/bling/installers/rpmfusion.sh
+++ modules/bling/installers/rpmfusion.sh
@@ -8,9 +8,17 @@ NEGATIVO_REPO_FILE="$(awk -F'=' '$1 == "name" && $2 == "negativo17 - Multimedia"
 # Check if rpmfusion is already installed before running
 if ! rpm -q rpmfusion-free-release &>/dev/null || ! rpm -q rpmfusion-nonfree-release &>/dev/null; then
   echo "Running RPMFusion repo install..."
-  rpm-ostree install \
-    "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm" \
-    "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm"
+  if ! rpm -q rpmfusion-free-release &>/dev/null && ! rpm -q rpmfusion-nonfree-release &>/dev/null; then
+    rpm-ostree install \
+      "https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm" \
+      "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm"
+  elif ! rpm -q rpmfusion-free-release &>/dev/null; then
+    rpm-ostree install \
+      "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-free-release-${OS_VERSION}.noarch.rpm"
+  elif ! rpm -q rpmfusion-nonfree-release &>/dev/null; then
+    rpm-ostree install \
+      "https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-${OS_VERSION}.noarch.rpm"
+  fi
   # check if negativo17 repo is installed
   if [[ -n "${NEGATIVO_REPO_FILE}" ]]; then
     echo "Making sure that Negativo17 repo is disabled"
