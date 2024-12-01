14308e0bff951c18dd723cee62b892932d662433
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-01 23:02:08 +0100

    fix(yafti): Populating custom flatpaks
    It's populated in reverse order compared to the format in recipe, but it works

--- modules/yafti/yafti.sh
+++ modules/yafti/yafti.sh
@@ -52,14 +52,19 @@ fi
 
 YAFTI_FILE="${FIRSTBOOT_DATA}/yafti.yml"
 
-get_json_array FLATPAKS 'try .["custom-flatpaks"][]' "$1"
+get_yaml_array FLATPAKS '.custom-flatpaks[]' "$1"
 if [[ ${#FLATPAKS[@]} -gt 0 ]]; then
     echo "Adding Flatpaks to yafti.yml"
-    sed -i '/^  Custom:/,/^  / s/description: .*/description: "Flatpaks suggested by the image maintainer."/' "${YAFTI_FILE}"
-    sed -i '/^  Custom:/,/^  / s/default: .*/default: true/' "${YAFTI_FILE}"
+    sed -i -e '/- Boatswain for Streamdeck: com.feaneron.Boatswain/a \
+        Custom:\n          description: Flatpaks suggested by the image maintainer.\n          default: true' "${YAFTI_FILE}"
 
     for pkg in "${FLATPAKS[@]}"; do
         echo "Adding to yafti: ${pkg}"
-        sed -i "/^  packages:/a\ \ - $pkg" "${YAFTI_FILE}"
+        sed -i '/^[[:space:]]*Custom:/ { 
+        n; n; n; 
+        i\
+            - REPLACEMEHERE
+    }' "${YAFTI_FILE}"    
+        sed -i "s/            - REPLACEMEHERE/            - ${pkg}/g" "${YAFTI_FILE}"
     done
 fi
