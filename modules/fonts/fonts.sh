9c4fd48679f27059d27a4079b4b6e61f9afe1b2e
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-01 23:40:19 +0100

    fix(fonts): Forgot to assign FONTS variable

--- modules/fonts/fonts.sh
+++ modules/fonts/fonts.sh
@@ -8,7 +8,7 @@ for SOURCE in "$MODULE_DIRECTORY"/fonts/sources/*.sh; do
     # get array of fonts for current source
     FILENAME=$(basename -- "${SOURCE}")
     ARRAY_NAME="${FILENAME%.*}"    
-    echo "${1}" | jq -c -r --arg ARRAY_NAME "${ARRAY_NAME}" 'try .[$ARRAY_NAME][]'
+    FONTS=("$(echo "${1}" | jq -c -r --arg ARRAY_NAME "${ARRAY_NAME}" 'try .[$ARRAY_NAME][]')")
 
     if [ ${#FONTS[@]} -gt 0 ]; then
         bash "${SOURCE}" "${FONTS[@]}"
