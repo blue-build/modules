66c0fd9228d91c8a6235390e8b5eba0f7845685f
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2024-12-01 20:53:30 +0100

    Update modules/files/files.sh
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- modules/files/files.sh
+++ modules/files/files.sh
@@ -28,8 +28,8 @@ if [[ ${#FILES[@]} -gt 0 ]]; then
         FILE="$PWD/$(echo $pair | jq -r 'to_entries | .[0].key')"
         DEST=$(echo $pair | jq -r 'to_entries | .[0].value')
       else
-        FILE="$PWD/$(echo $pair | jq -r 'try .["source"]')"
-        DEST=$(echo $pair | jq -r 'try .["destination"]')
+        FILE="$PWD/$(echo "$pair" | jq -r 'try .["source"]')"
+        DEST=$(echo "$pair" | jq -r 'try .["destination"]')
       fi 
         if [ -d "$FILE" ]; then
             if [ ! -d "$DEST" ]; then
