c86fc62b7dca829f119056f122aebcdc4e54a218
xyny <git@xyny.anonaddy.me>
2025-06-30 13:31:25 +0300

    fix: stop using yq & update signing module

--- modules/signing/signing.sh
+++ modules/signing/signing.sh
@@ -6,52 +6,67 @@ set -euo pipefail
 if grep -q OSTREE /etc/os-release; then
     echo "Detected OSTREE"
 
-    CONTAINER_DIR="/usr/etc/containers"
+    CONTAINER_DIR="/etc/containers"
     MODULE_DIRECTORY="${MODULE_DIRECTORY:-"/tmp/modules"}"
     IMAGE_NAME_FILE="${IMAGE_NAME//\//_}"
 
-    echo "Setting up container signing in policy.json and cosign.yaml for $IMAGE_NAME"
-    echo "Registry to write: $IMAGE_REGISTRY"
+    echo "Setting up container signing in policy.json and cosign.yaml for ${IMAGE_NAME}"
+    echo "Registry to write: ${IMAGE_REGISTRY}"
 
-    if ! [ -d "$CONTAINER_DIR" ]; then
-        mkdir -p "$CONTAINER_DIR"
+    if ! [ -d "${CONTAINER_DIR}" ]; then
+        mkdir -p "${CONTAINER_DIR}"
     fi
 
-    if ! [ -d $CONTAINER_DIR/registries.d ]; then
-    mkdir -p "$CONTAINER_DIR/registries.d"
+    if ! [ -d "${CONTAINER_DIR}/registries.d" ]; then
+        mkdir -p "${CONTAINER_DIR}/registries.d"
     fi
 
-    if ! [ -d "/usr/etc/pki/containers" ]; then
-        mkdir -p "/usr/etc/pki/containers"
+    if ! [ -d "/etc/pki/containers" ]; then
+        mkdir -p "/etc/pki/containers"
     fi
 
-    if ! [ -f "$CONTAINER_DIR/policy.json" ]; then
-        cp "$MODULE_DIRECTORY/signing/policy.json" "$CONTAINER_DIR/policy.json"
+    if ! [ -f "/etc/pki/containers/${IMAGE_NAME_FILE}.pub" ]; then
+        echo "ERROR: Cannot find '${IMAGE_NAME_FILE}.pub' image key in '/etc/pki/containers/'"
+        echo "       BlueBuild CLI should have copied it, but it didn't"
+        exit 1
     fi
 
-    if ! [ -f "/usr/etc/pki/containers/$IMAGE_NAME_FILE.pub" ]; then
-        cp "/usr/share/ublue-os/cosign.pub" "/usr/etc/pki/containers/$IMAGE_NAME_FILE.pub"
+    TEMPLATE_POLICY="${MODULE_DIRECTORY}/signing/policy.json"
+    POLICY_FILE="${CONTAINER_DIR}/policy.json"
+
+    # If there is no policy.json file, then copy the template policy
+    if ! [ -f "${POLICY_FILE}" ]; then
+        cp "${TEMPLATE_POLICY}" "${POLICY_FILE}"
     fi
 
-    POLICY_FILE="$CONTAINER_DIR/policy.json"
+    # If the already existing policy.json file doesn't have 'reject' as default policy,
+    # then signing is effectively disabled & template policy.json should be copied in that case also
+    if [[ "$(jq -r '.default[0].type' "${POLICY_FILE}")" == "insecureAcceptAnything" ]]; then
+        cp "${TEMPLATE_POLICY}" "${POLICY_FILE}"
+    fi
 
-    yq -i -o=j '.transports.docker |=
-        {"'"$IMAGE_REGISTRY"'/'"$IMAGE_NAME"'": [
-                {
-                    "type": "sigstoreSigned",
-                    "keyPath": "/usr/etc/pki/containers/'"$IMAGE_NAME_FILE"'.pub",
-                    "signedIdentity": {
-                        "type": "matchRepository"
-                    }
+    jq --arg image_registry "${IMAGE_REGISTRY}" \
+    --arg image_name "${IMAGE_NAME}" \
+    --arg image_name_file "${IMAGE_NAME_FILE}" \
+    '.transports.docker |= 
+        { ($image_registry + "/" + $image_name): [
+            {
+                "type": "sigstoreSigned",
+                "keyPath": ("/etc/pki/containers/" + $image_name_file + ".pub"),
+                "signedIdentity": {
+                    "type": "matchRepository"
                 }
-            ]
-        }
-    + .' "$POLICY_FILE"
+            }
+        ] } + .' "${POLICY_FILE}" > "/tmp/POLICY.tmp"
 
-    mv "$MODULE_DIRECTORY/signing/registry-config.yaml" "$CONTAINER_DIR/registries.d/$IMAGE_NAME_FILE.yaml"
-    sed -i "s ghcr.io/IMAGENAME $IMAGE_REGISTRY g" "$CONTAINER_DIR/registries.d/$IMAGE_NAME_FILE.yaml"
+    mv "/tmp/POLICY.tmp" "${POLICY_FILE}"
+
+    mv "${MODULE_DIRECTORY}/signing/registry-config.yaml" "${CONTAINER_DIR}/registries.d/${IMAGE_REGISTRY##*/}-${IMAGE_NAME_FILE}.yaml"
+    sed -i "s ghcr.io/IMAGENAME ${IMAGE_REGISTRY}/${IMAGE_NAME} g" "${CONTAINER_DIR}/registries.d/${IMAGE_REGISTRY##*/}-${IMAGE_NAME_FILE}.yaml"
+
 elif grep -q "Vanilla OS" /etc/os-release; then
     echo "Detected Vanilla OS"
+
     USERNAME="${IMAGE_REGISTRY##*/}"
     jq -r ".name |= \"$USERNAME/$IMAGE_NAME\"" /usr/share/abroot/abroot.json > /usr/share/abroot/abroot_tmp.json
     mv /usr/share/abroot/abroot_tmp.json /usr/share/abroot/abroot.json
