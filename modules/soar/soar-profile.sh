03930ae933f10bd787e6f9d39754b926d35c4bde
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-07-26 15:19:01 +0200

    chore(soar): Add condition in shell profile to check if `soar` PATH already exists

--- modules/soar/soar-profile.sh
+++ modules/soar/soar-profile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Don't source PATH for soar packages when it's not an interactive terminal session & when it's a root user
 if [[ ${-} == *i* && "$(/bin/id -u)" != 0 ]]; then
-  export PATH="${XDG_DATA_HOME:-$HOME/.local/share}/soar/bin:${PATH}"
+  if ! [[ "$PATH" =~ "${XDG_DATA_HOME:-$HOME/.local/share}/soar/bin:" ]]; then
+    export PATH="${XDG_DATA_HOME:-$HOME/.local/share}/soar/bin:${PATH}"
+  fi
 fi
