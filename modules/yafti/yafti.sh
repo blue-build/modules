f27a08bee415f0f7ff33d289f199d02b42a4d560
fiftydinar <65243233+fiftydinar@users.noreply.github.com>
2025-11-20 18:37:40 +0100

    chore(yafti): Add useful logs

--- modules/yafti/yafti.sh
+++ modules/yafti/yafti.sh
@@ -5,39 +5,37 @@ set -euo pipefail
 
 MODULE_DIRECTORY="${MODULE_DIRECTORY:-"/tmp/modules"}"
 
+echo "Copying default yafti.yml config if not present"
 FIRSTBOOT_DATA="/usr/share/ublue-os/firstboot"
-
 mkdir -p "$FIRSTBOOT_DATA/launcher/"
-
 # doesn't overwrite user's yafti.yml
 cp --update=none "$MODULE_DIRECTORY/yafti/yafti.yml" "$FIRSTBOOT_DATA/yafti.yml"
-cp -r "$MODULE_DIRECTORY/yafti/launcher/" "$FIRSTBOOT_DATA"
 
+echo "Copying and configuring yafti autostart config"
+cp -r "$MODULE_DIRECTORY/yafti/launcher/" "$FIRSTBOOT_DATA"
 FIRSTBOOT_SCRIPT="${FIRSTBOOT_DATA}/launcher/login-profile.sh"
 PROFILED_DIR="/etc/profile.d"
 FIRSTBOOT_LINK="${PROFILED_DIR}/ublue-firstboot.sh"
+# If the profile.d directory doesn't exist, create it
+if [ ! -d "${PROFILED_DIR}" ]; then
+    mkdir -p "${PROFILED_DIR}"
+fi
+# Create symlink to our profile script, which creates the per-user "autorun yafti" links.
+if [ -f "${FIRSTBOOT_SCRIPT}" ]; then
+    ln -sf "${FIRSTBOOT_SCRIPT}" "${FIRSTBOOT_LINK}"
+fi
 
 # Install yafti
+echo "Installing yafti"
 YAFTI_REPO="https://github.com/fiftydinar/Yafti-AppImage"
 ARCH="$(uname -m)"
 VER=$(basename $(curl -Ls -o /dev/null -w %{url_effective} "$YAFTI_REPO"/releases/latest))
 curl -fLs --create-dirs "$YAFTI_REPO/releases/download/${VER}/yafti-${VER%@*}-anylinux-${ARCH}.AppImage" -o /usr/bin/yafti
 chmod +x /usr/bin/yafti
 
-# If the profile.d directory doesn't exist, create it
-if [ ! -d "${PROFILED_DIR}" ]; then
-    mkdir -p "${PROFILED_DIR}"
-fi
-
-# Create symlink to our profile script, which creates the per-user "autorun yafti" links.
-if [ -f "${FIRSTBOOT_SCRIPT}" ]; then
-    ln -sf "${FIRSTBOOT_SCRIPT}" "${FIRSTBOOT_LINK}"
-fi
-
-YAFTI_FILE="${FIRSTBOOT_DATA}/yafti.yml"
-
 get_json_array FLATPAKS 'try .["custom-flatpaks"][]' "${1}"
 if [[ ${#FLATPAKS[@]} -gt 0 ]]; then
+    YAFTI_FILE="${FIRSTBOOT_DATA}/yafti.yml"
     echo "Adding Flatpaks to yafti.yml"
     sed -i -e '/- Boatswain for Streamdeck: com.feaneron.Boatswain/a \
         Custom:\n          description: Flatpaks suggested by the image maintainer.\n          default: true' "${YAFTI_FILE}"
